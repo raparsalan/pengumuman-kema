7178bd64c6da04f090584f3b28224f9e879b1ab9
Rafi Arsalan <anburoot712@gmail.com>
2024-07-14 01:54:02 +0700

    Add files via upload

---
+++ assets/js/announcement.js
@@ -0,0 +1,140 @@
+let pengurus = [
+    {id:1, nim: "2304035", nama :"Ajeng Tia Vijola", idBidang:0, img:"1.png"},
+    {id:2, nim: "2309011", nama :"Alya Putri Ramadhani", idBidang:0, img:"2.png"},
+    {id:3, nim: "2301568", nama :"Diana Farida", idBidang:0, img:"3.png"},
+    {id:4, nim: "2206855", nama :"Ira Kamilia", idBidang:0, img:"4.png"},
+    {id:5, nim: "2306122", nama :"Levina Lidya Maheswari", idBidang:0, img:"5.png"},
+    {id:6, nim: "2306848", nama :"Muhamad Taufik Hidayat", idBidang:0, img:"6.png"},
+    {id:7, nim: "2306173", nama :"Nasywa Putri Ramadhina", idBidang:0, img:"7.png"},
+    {id:8, nim: "2312179", nama :"Natasya Awlya", idBidang:0, img:"8.png"},
+    {id:9, nim: "2306071", nama :"Tisdiawati Intan Pandini", idBidang:0, img:"9.png"},
+    {id:10, nim: "2303612", nama :"Aurylia Taffana", idBidang:1, img:"10.png"},
+    {id:11, nim: "2107609", nama :"Dewi Nudlrotul Uyun", idBidang:1, img:"11.png"},
+    {id:12, nim: "2200860", nama :"Imas Sulastri", idBidang:1, img:"12.png"},
+    {id:13, nim: "2207231", nama :"Yohanes Peter Adam", idBidang:1, img:"13.png"},
+    {id:14, nim: "2308848", nama :"Desti Septiani", idBidang:2, img:"14.png"},
+    {id:15, nim: "2309324", nama :"Fairuzzahwa Fatimah Nurdini", idBidang:2, img:"15.png"},
+    {id:16, nim: "2300117", nama :"Hanifah El Faizah", idBidang:2, img:"16.png"},
+    {id:17, nim: "2305901", nama :"Irham Muhammad Faqqih", idBidang:2, img:"17.png"},
+    {id:18, nim: "2305103", nama :"Isma Yunisa", idBidang:2, img:"18.png"},
+    {id:19, nim: "2307710", nama :"Raina Zhafira Ruliandi", idBidang:2, img:"19.png"},
+    {id:20, nim: "2309314", nama :"Rifa Hilman Mubarok", idBidang:2, img:"20.png"},
+    {id:21, nim: "2109193", nama :"Suci Indah Ramdhani", idBidang:2, img:"21.png"},
+    {id:22, nim: "2301443", nama :"Syafa Salsa Khoirunnisa", idBidang:2, img:"22.png"},
+    {id:23, nim: "2306962", nama :"Ade Maulana", idBidang:3},
+    {id:24, nim: "2304500", nama :"Alfizidan Fiqri Pratama", idBidang:3},
+    {id:25, nim: "2305800", nama :"Dhara Almira Desideria", idBidang:3},
+    {id:26, nim: "2301519", nama :"Erdyanto Matthew Hasiholan Munthe", idBidang:3},
+    {id:27, nim: "2301641", nama :"Fairuz Naila Putri Irawan Kartadimadja", idBidang:3},
+    {id:28, nim: "2204935", nama :"Ghina Nur Rohmah", idBidang:3},
+    {id:29, nim: "2201733", nama :"Halwa Anisa Zaitun", idBidang:3},
+    {id:30, nim: "2306834", nama :"Hasya Afifah Hannan", idBidang:3},
+    {id:31, nim: "2304344", nama :"Nuraisya Rahmadina Putri", idBidang:3},
+    {id:32, nim: "2305047", nama :"Oliviani Eka Rasmuni", idBidang:3},
+    {id:33, nim: "2301767", nama :"Raihan Dzaky Antariksa Putra", idBidang:3},
+    {id:34, nim: "2210163", nama :"Siti Hajar Pamungkas", idBidang:3},
+    {id:35, nim: "2305632", nama :"Devan Nadif Rizki Hidayat", idBidang:3},
+    {id:36, nim: "2301890", nama :"Dhina Oktavia Ramadhan Nurlita Zaman", idBidang:4},
+    {id:37, nim: "2310037", nama :"Heri Dwi Prasetyo", idBidang:4},
+    {id:38, nim: "2302069", nama :"Muhammad Salman Naufal", idBidang:4},
+    {id:39, nim: "2306142", nama :"Raihan Zaky Al Fatah", idBidang:4},
+    {id:40, nim: "2305902", nama :"Salma Azzahra", idBidang:4},
+    {id:41, nim: "2305934", nama :"Salsabila Zahra Sugiono", idBidang:4},
+    {id:42, nim: "2300606", nama :"Shabrina Zainuba Azahra", idBidang:4},
+    {id:43, nim: "2307790", nama :"Riza Natania Zulyatina", idBidang:4},
+    {id:44, nim: "2300492", nama :"Ahmad Izzuddin Azzam", idBidang:5},
+    {id:45, nim: "2304087", nama :"Alya Zahra Nurul Inayah", idBidang:5},
+    {id:46, nim: "2303876", nama :"Anastasia Dwi Febrianty", idBidang:5},
+    {id:47, nim: "2202794", nama :"Dinda Triyani Putri", idBidang:5},
+    {id:48, nim: "2207204", nama :"Hilda Muthia Puspa", idBidang:5},
+    {id:49, nim: "2311544", nama :"Hurul Aini", idBidang:5},
+    {id:51, nim: "2306192", nama :"Husna Alifah Muthmainnah", idBidang:5},
+    {id:52, nim: "2300484", nama :"Julian Dwi Satrio", idBidang:5},
+    {id:53, nim: "2203027", nama :"Raizza Meisitta Maulia", idBidang:5},
+    {id:54, nim: "2301136", nama :"Rasyiqa Zata Dini", idBidang:5},
+    {id:55, nim: "2202025", nama :"Ridwan Assidik", idBidang:5},
+    {id:56, nim: "2307259", nama :"Shevina Triana", idBidang:5},
+    {id:57, nim: "2205085", nama :"Syifa Nabilah Az-zahra", idBidang:5},
+    {id:58, nim: "2207242", nama :"Tyas Nur'aafiyah Ramdhani", idBidang:5},
+    {id:59, nim: "2200720", nama :"Wina Marlena", idBidang:5},
+    {id:60, nim: "2305623", nama :"Abdul Hakim Assonhaji", idBidang:6},
+    {id:61, nim: "2306851", nama :"Bevan Abdallah", idBidang:6},
+    {id:62, nim: "2300616", nama :"Candra Putra Budianto", idBidang:6},
+    {id:63, nim: "2307876", nama :"Muhamad Hardiansyah Nur Arifin", idBidang:6},
+    {id:64, nim: "2308882", nama :"Narendra Ridha Baihaqi", idBidang:6},
+    {id:65, nim: "2301325", nama :"Nashaliya Hasifa Yonvitra", idBidang:6},
+    {id:66, nim: "2304931", nama :"Rabbani Ghani Firdaus", idBidang:6},
+    {id:67, nim: "2310435", nama :"Shafa Dzakira Az-Zahra", idBidang:6},
+    {id:68, nim: "2307960", nama :"Zita Musfirah Aziz", idBidang:6},
+    {id:69, nim: "2204350", nama :"Azzahra Salsabila", idBidang:7},
+    {id:70, nim: "2305949", nama :"Cantika Dwi Mutiara Putri", idBidang:7},
+    {id:71, nim: "2203582", nama :"Devindra Kevin Andriyoga", idBidang:7},
+    {id:72, nim: "2305004", nama :"Farhah Nadhif Tsalist", idBidang:7},
+    {id:73, nim: "2201648", nama :"Ihsanti Kamilah", idBidang:7},
+    {id:74, nim: "2308831", nama :"Mela Meilawati", idBidang:7},
+    {id:75, nim: "2306987", nama :"Muhammad Anwar Hidayat", idBidang:7},
+    {id:76, nim: "2308183", nama :"Nafida Aufiyassalma Kautsary", idBidang:7},
+    {id:77, nim: "2304513", nama :"Phasya Aulia Syafira", idBidang:7},
+    {id:78, nim: "2204044", nama :"Riesa Riskania Yuniman", idBidang:7},
+    {id:79, nim: "2301620", nama :"Syaira Salsabila", idBidang:7},
+    {id:80, nim: "2307774", nama :"Wanda Fatoni Putri", idBidang:7},
+    {id:81, nim: "2203199", nama :"Abelia Rahmah Oktaviani", idBidang:8},
+    {id:82, nim: "2303542", nama :"Danda Aditya", idBidang:8},
+    {id:83, nim: "2311024", nama :"Fadhlan Fauzil Fakhruddin", idBidang:8},
+    {id:84, nim: "2204745", nama :"Fahira Hapsari", idBidang:8},
+    {id:85, nim: "2204857", nama :"Kaila Ayuning Tyas", idBidang:8},
+    {id:86, nim: "2202807", nama :"Lani Nur Patria", idBidang:8},
+    {id:87, nim: "2209798", nama :"Risya Jheniar Nur Siti Khamilah", idBidang:8},
+    {id:88, nim: "2306887", nama :"Salsa Julia Ningrum", idBidang:8},
+    {id:89, nim: "2200179", nama :"Adinda Nurazizah", idBidang:9},
+    {id:90, nim: "2103864", nama :"Alviana Indriyani Surachman", idBidang:9},
+    {id:91, nim: "2207163", nama :"Alvina Hardiyanti", idBidang:9},
+    {id:92, nim: "2308043", nama :"Amilah Munadziroh", idBidang:9},
+    {id:93, nim: "2200458", nama :"Ersa Meilia", idBidang:9},
+    {id:94, nim: "2207045", nama :"Muhammad Alfi Faiz", idBidang:9},
+    {id:95, nim: "2304344", nama :"Nuraisya Rahmadina Putri", idBidang:9},
+    {id:96, nim: "2303380", nama :"Robi Nuriman", idBidang:9},
+    {id:97, nim: "2204725", nama :"Salsabila Yasfa Khairunnisa", idBidang:9},
+    {id:98, nim: "2201056", nama :"Silviani Despitasari", idBidang:9},
+    {id:99, nim: "2101958", nama :"Syifa Qolbiah", idBidang:9},
+]
+let bidang = [
+    "Bidang Kesekretariatan Umum",
+    "Bidang Keuangan",
+    "Bidang Komunikasi dan Informasi",
+    "Bidang Akademik dan Profesi",
+    "Bidang Kerohanian",
+    "Bidang Minat dan Bakat",
+    "Bidang Pengembangan Internal",
+    "Bidang Pengembangan Sumber Daya Organisasi",
+    "Bidang Advokasi Kesejahteraan Mahasiswa",
+    "Bidang Sosial dan Politik",
+]
+
+let namaPengurus = document.getElementById("nama-ann")
+let nimPengurus = document.getElementById("nim-ann")
+let greetings = document.getElementById("greetings")
+let bidangPengurus = document.getElementById("bidang-ann")
+let profilePic = document.getElementById("profile-pic")
+let urlParams = new URLSearchParams(window.location.search);
+let id = urlParams.get('id');
+let unboxingDate = new Date(2024,6,17,15,0)
+
+window.onload = function(){
+    let person = pengurus.find(p => p.id == id);
+    if (person) {
+        let currentDate = new Date()
+        console.log(unboxingDate)
+        console.log(currentDate)
+        if(currentDate >= unboxingDate){
+            let _bidang = bidang[person.idBidang]
+            namaPengurus.textContent = person.nama
+            nimPengurus.textContent = person.nim
+            profilePic.src ="images/profile/"+ person.img
+            bidangPengurus.textContent = "Selamat Datang di " + _bidang
+        }else{
+            greetings.textContent = "DIBILANG JANGAN DIBUKA DULUAN"
+        }
+    }
+    console.log(id)
+};
